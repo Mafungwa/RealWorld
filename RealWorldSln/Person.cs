9589c56f71c711040b646074d8c22e81145ec8d9
Mafungwa <33479523+Mafungwa@users.noreply.github.com>
2023-12-08 16:18:38 +0200

    first commit

---
+++ RealWorldSln/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealWorldSln
+{
+    internal class Person
+    {
+        
+        private int Age;
+        private string Race;
+        private double Height;
+        private string Name;
+        private bool HaveMatric;
+        private bool Employeed;
+
+
+        public Person(string name, int age, string race, double height, bool employeed, bool havematric) {
+
+            Name = name;
+            Age = age;
+            Race = race;
+            Height = height;
+            HaveMatric = havematric;
+            Employeed = employeed;        
+        }
+
+        public string Run(string relay)
+        {
+            return relay;
+        }
+        public string Speak(string language)
+        {
+            return language;
+        }
+        public bool Medication(bool status) 
+        {
+            return true;
+        }
+    }
+}
---
+++ RealWorldSln/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RealWorldSln
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+
+        Person person1 = new Person("Pule", 30, "Black", 1.8, false, true);
+
+            person1.Speak("IsiXosha");
+            person1.Run("100m");
+            person1.Medication(true);
+
+            Console.WriteLine("Personal Information: " + person1.Name);
+            Console.WriteLine("Personal Information: " + person1.Age);
+            Console.WriteLine("Personal Information: " + person1.Race);
+            Console.WriteLine("Personal Information: " + person1.Height);
+            Console.WriteLine("Personal Information: " + person1.Employeed);
+            Console.WriteLine("Personal Information: " + person1.HaveMatric);
+            Console.WriteLine("Personal Information: " + person1.Speak);
+            Console.WriteLine("Personal Information: " + person1.Run);
+            Console.WriteLine("Personal Information: " + person1.Medication);
+
+
+
+
+
+        }
+    }
+}
